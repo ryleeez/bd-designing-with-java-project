1461c8afbbccbbec72a58a46efae914dacb06f70
FrankJFella <89809152+FrankJFella@users.noreply.github.com>
2022-02-21 14:56:20 -0500

    Update PackagingTest.java

--- tst/com/amazon/ata/types/PackagingTest.java
+++ tst/com/amazon/ata/types/PackagingTest.java
@@ -175,4 +175,11 @@ public class PackagingTest {
         // WHEN + THEN
         assertEquals(packaging.hashCode(), other.hashCode(), "Equal objects should have equal hashCodes");
     }
+    @Test
+    public void height_width_length_variables_removed_from_packaging_class() {
+        Field[] fields = Packaging.class.getDeclaredFields();
+        assertNotEquals(true, Arrays.stream(fields).anyMatch(f -> f.getName().equals("height")), "height attribute should be removed from Packaging class since it was moved to Box class");
+        assertNotEquals(true, Arrays.stream(fields).anyMatch(f -> f.getName().equals("width")),  "width attribute should be removed from Packaging class since it was moved to Box class");
+        assertNotEquals(true, Arrays.stream(fields).anyMatch(f -> f.getName().equals("length")), "length attribute should be removed from Packaging class since it was moved to Box class");
+        }
 }
